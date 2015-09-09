45b14eb81d4c3a36ecdfa646c8137f7a7d545c56
Tatiana Kravetc <dallord@yandex.ru>
2015-09-09 12:59:20 +0000

    detect main reference point for each line

--- learning/mmm/main.cpp
+++ learning/mmm/main.cpp
@@ -3,33 +3,75 @@
 #include <fstream>
 #include <iostream>
 
+#include <vector>
+
 using namespace std;
+
+int w = 80;
+int h = 60;
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     string buff; //line buffer
-    int *ss; //numbers
+    int ss; //numbers
+    char temp[4];
 
-    ifstream file("../data/outData.txt"); //open data-file
+    long counter = 0;
+
+    vector<int> vct;
+
+    ifstream file("../../data/outData.txt"); //open data-file
 
     if (file.is_open()){
-        file.seekg (0, ios::end);
-        long fileSize = file.tellg();
-        file.seekg(0, ios::beg);
 
-        ss = (int*) malloc (fileSize*sizeof(int));
-
-        file.getline(buff, 24000); //reading first line
-
-        for (int i = 0; i <= buff.Length(); i++){
-            if (buff[i] != " "){
-
-            }
+        while(!file.eof()){
+            file >> ss;
+            vct.push_back(ss);
         }
 
-        file.close();
+
+        //cout << vct.size() << endl;
+    }else{
+        cout << "Unable to open file" << endl;
     }
+    file.close();
+
+    file.open("../../data/outData.txt");
+    while (!file.eof()){
+        getline(file, buff);
+        counter++;
+    }
+    file.close();
+
+
+    vector<int> data[counter]; //data array
+
+    for (int j = 0; j < counter; j++){
+        for(int i = 0; i < vct.size()/counter; i++){
+            data[j].push_back(vct[j*vct.size()/counter+i]);
+        }
+    }
+
+    //find coordinates of maximum value in each line
+    int max;
+    int x, y;
+    for (int j = 0; j < counter; j++){
+        max = 8000; //detect object
+        x = 0;
+        y = 0;
+        for (int i = 0; i < vct.size()/counter; i++){
+            if (data[j][i] > max){
+                max = data[j][i];
+                x = i%w;
+                y = i/w;
+            }
+        }
+        if (max != 8000)
+            cout << max << ": " << x << " " << y << endl;
+        else cout << "no object" << endl;
+    }
+
 
 
     return a.exec();
