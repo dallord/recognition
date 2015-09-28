af730f9247a9727606686757558f489ffeff9d6e
Tatiana Kravetc <dallord@yandex.ru>
2015-09-28 13:57:58 +0000

    learning tests

--- learning/lll/main.cpp
+++ learning/lll/main.cpp
@@ -5,9 +5,9 @@
 #include <iostream>
 
 #define N 6 //number of components of X vector
-#define M 3 //number of components of Y vector
+#define M 4 //number of components of Y vector
 
-#define K 4 //number of classes
+#define K 5 //number of classes
 
 using namespace std;
 
@@ -24,6 +24,7 @@ struct Y{ //otput vector of states
     float s0;
     float s1;
     float s2;
+    float s3;
 
     int type;
 };
@@ -39,7 +40,7 @@ X* learning_in; //learning set
 Y classes[K];
 
 float** r; //array of distances
-float gam[7]; //array of gamma coefficients
+float* gam; //array of gamma coefficients
 float h; //width of the window
 
 void init_classes(){ //init array of existing classes
@@ -48,32 +49,45 @@ void init_classes(){ //init array of existing classes
     classes[0].s0 = 0.;
     classes[0].s1 = 0.;
     classes[0].s2 = 0.;
+    classes[0].s3 = 0.;
     classes[0].type = 0;
 
     //"standing"
     classes[1].s0 = 1.;
     classes[1].s1 = 0.;
     classes[1].s2 = 0.;
+    classes[1].s3 = 0.;
     classes[1].type = 1;
 
     //"sitting"
     classes[2].s0 = 0.;
     classes[2].s1 = 1.;
     classes[2].s2 = 0.;
+    classes[2].s3 = 0.;
     classes[2].type = 2;
 
-    //"lying"
+    //"lying on the bed"
     classes[3].s0 = 0.;
     classes[3].s1 = 0.;
     classes[3].s2 = 1.;
+    classes[3].s3 = 0.;
     classes[3].type = 3;
+
+    //"lying on the floor"
+    classes[4].s0 = 0.;
+    classes[4].s1 = 0.;
+    classes[4].s2 = 0.;
+    classes[4].s3 = 1.;
+    classes[4].type = 4;
 }
 
+
 void set_type(Y y){
-    if ((y.s0 = 0) && (y.s1 = 0) && (y.s2 = 0)) y.type = 0;
-    if ((y.s0 = 1) && (y.s1 = 0) && (y.s2 = 0)) y.type = 1;
-    if ((y.s0 = 0) && (y.s1 = 1) && (y.s2 = 0)) y.type = 2;
-    if ((y.s0 = 0) && (y.s1 = 0) && (y.s2 = 1)) y.type = 3;
+    if ((y.s0 = 0) && (y.s1 = 0) && (y.s2 = 0) && (y.s3 = 0)) y.type = 0;
+    if ((y.s0 = 1) && (y.s1 = 0) && (y.s2 = 0) && (y.s3 = 0)) y.type = 1;
+    if ((y.s0 = 0) && (y.s1 = 1) && (y.s2 = 0) && (y.s3 = 0)) y.type = 2;
+    if ((y.s0 = 0) && (y.s1 = 0) && (y.s2 = 1) && (y.s3 = 0)) y.type = 3;
+    if ((y.s0 = 0) && (y.s1 = 0) && (y.s2 = 0) && (y.s3 = 1)) y.type = 4;
 }
 
 float rho(X u, X x_i){ //distance
@@ -106,16 +120,14 @@ void qsort(float rho[], int left, int right){
 float algorithm(X u){
     int argmax = 0; //will return argument of maximum value
     float max = 0; //maximum value
-    int b; //true or false
     for (int e = 0; e < K; e++){
         float sum = 0;
         for (int i = 0; i < counter_train; i++){
             if (    (training_out[i].s0 == classes[e].s0) &&
                     (training_out[i].s1 == classes[e].s1) &&
-                    (training_out[i].s2 == classes[e].s2))
-                b = 1;
-            else b = 0;
-            sum += b*gam[i]*(h/rho(u, training_in[i]));
+                    (training_out[i].s2 == classes[e].s2) &&
+                    (training_out[i].s3 == classes[e].s3))
+                sum += gam[i]*(h/(rho(u, training_in[i])+1.));
         }
         if (sum > max){
             max = sum;
@@ -126,10 +138,11 @@ float algorithm(X u){
 }
 
 void init_gamma(){
+    gam = new float[counter_train];
     for (int i = 0; i < counter_train; i++){
-        gam[i] = 0;
-        if (algorithm(training_in[i]) != training_out[i].type)
-            gam[i] += 1;
+        gam[i] = 1;
+        //if (algorithm(training_in[i]) != training_out[i].type)
+         //   gam[i] += 1;
     }
 }
 
@@ -139,8 +152,8 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
 
 
-    ifstream infilelearn("../../data/ref_points.txt"); //open list of reference points for learning
-    ifstream infiletrain("../../data/training_set.txt"); //open file with training set
+    ifstream infilelearn("../../data/experiments/test2_ref.txt"); //open list of reference points for learning
+    ifstream infiletrain("../../data/experiments/test0_pairs.txt"); //open file with training set
 
     ofstream outfile("../../data/result.txt"); //output file
 
@@ -169,31 +182,31 @@ int main(int argc, char *argv[])
 
     //gam = new float[counter_train];
     init_gamma();
-    h = 5;
+    h = 3;
 
     learning_in = new X[counter_learn];
     init_classes();
 
     //filling training and learning sets
-    infiletrain.open("../../data/training_set.txt");
+    infiletrain.open("../../data/experiments/test0_pairs.txt");
     string line;
     int i = 0;
     while(!infiletrain.eof()){
         getline(infiletrain, line);
-        sscanf(line.c_str(), "%d %d %d %d %d %d %f %f %f",
+        sscanf(line.c_str(), "%d %d %d %d %d %d %f %f %f %f",
                &training_in[i].x0, &training_in[i].y0, &training_in[i].x1, &training_in[i].y1, &training_in[i].x2, &training_in[i].y2,
-               &training_out[i].s0, &training_out[i].s1, &training_out[i].s2);
+               &training_out[i].s0, &training_out[i].s1, &training_out[i].s2, &training_out[i].s3);
         set_type(training_out[i]);
         i++;
     }
     infiletrain.close();
 
 
-    infilelearn.open("../../data/ref_points.txt");
+    infilelearn.open("../../data/experiments/test2_ref.txt");
     i = 0;
     while(!infilelearn.eof()){
         getline(infilelearn, line);
-        sscanf(line.c_str(), "%d %d %d %d %d %d",
+        sscanf(line.c_str(), "%*s %*s %d %d %d %d %d %d",
                &learning_in[i].x0, &learning_in[i].y0, &learning_in[i].x1, &learning_in[i].y1, &learning_in[i].x2, &learning_in[i].y2);
         i++;
     }
@@ -230,7 +243,8 @@ int main(int argc, char *argv[])
         if (A[i] == 0) classname = "no object";
         if (A[i] == 1) classname = "standing";
         if (A[i] == 2) classname = "sitting";
-        if (A[i] == 3) classname = "lying";
+        if (A[i] == 3) classname = "lying on the bed";
+        if (A[i] == 4) classname = "lying on the floor";
         outfile << learning_in[i].x0 << " " << learning_in[i].y0 << " " << learning_in[i].x1 << " " << learning_in[i].y1 << " " << learning_in[i].x2 << " " << learning_in[i].y2 << " = " << classname << endl;
     }
     outfile.close();
